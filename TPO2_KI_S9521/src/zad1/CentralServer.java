a0086ef3cc4044373668416f3d15b64f0e0e0415
ikownacki <kownak.tag@gmail.com>
2017-03-19 23:09:56 +0100

    ZAD 2 dodanie gui

---
+++ TPO2_KI_S9521/src/zad1/CentralServer.java
@@ -0,0 +1,12 @@
+package zad1;
+
+/**
+ * Created by ikownacki on 19.03.2017.
+ */
+public class CentralServer implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ TPO2_KI_S9521/src/zad1/DictionaryServer.java
@@ -0,0 +1,18 @@
+package zad1;
+
+import java.net.ServerSocket;
+import java.nio.file.Path;
+
+/**
+ * Created by ikownacki on 19.03.2017.
+ */
+public class DictionaryServer implements Runnable {
+    Path dictionaryFile;
+    ServerSocket serverSocket;
+
+    @Override
+    public void run() {
+
+    }
+
+}
