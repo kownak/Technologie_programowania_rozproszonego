bae11c9b72818421aad32122326df25fa7bfdc22
ikownacki <kownak.tag@gmail.com>
2017-04-11 23:14:27 +0200

    Zad3 central server in progress

--- TPO3_KI_S9521/src/Zad1/CentralServer.java
+++ TPO3_KI_S9521/src/Zad1/CentralServer.java
@@ -2,9 +2,13 @@ package Zad1;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by ikownacki on 05.04.2017.
@@ -27,9 +31,42 @@ public class CentralServer {
             e.printStackTrace();
             System.exit(1);
         }
+        serviceConnections();
     }
 
-    public void serviceConnections(){
+    private void serviceConnections(){
+
+        while (true) {
+            try {
+
+                selector.select();
+                Set<SelectionKey> keys = selector.keys();
+                ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+
+                Iterator<SelectionKey> iterator = keys.iterator();
+                while ((iterator.hasNext())){
+                    SelectionKey selectionKey = iterator.next();
+                    keys.remove(selectionKey);
+
+                    if(selectionKey.isAcceptable()){
+                        SocketChannel socketChannel = serverSocketChannel.accept();
+                        socketChannel.configureBlocking(false);
+                        socketChannel.register(selector,SelectionKey.OP_READ);
+                    }
+                    if(selectionKey.isReadable()){
+                        SocketChannel client = (SocketChannel) selectionKey.channel();
+                        client.read(byteBuffer);
+
+                    }
+                    if(selectionKey.isWritable()){
+
+                    }
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
